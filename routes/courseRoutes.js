35f09c291b0d487246bd8bf6f23bc31393c1c24a
JohannaHannahoJ <joha2102@student.miun.se>
2026-06-03 09:03:24 +0200

    Add delete course endpoint

--- routes/courseRoutes.js
+++ routes/courseRoutes.js
@@ -33,8 +33,6 @@ router.post("/", authenticateToken, async (req, res) => {
             return res.status(400).json({ message: "Välj en kurs!" });
         }
 
-        // lägg till validering för at kolla om kursen reedna finns..
-
         await client.query(
             "INSERT INTO courses (user_id, course_code, course_name, points, subject, syllabus) VALUES ($1, $2, $3, $4, $5, $6)", [userId, courseCode, courseName, points, subject, syllabus]
         );
@@ -42,7 +40,7 @@ router.post("/", authenticateToken, async (req, res) => {
         res.status(201).json({ message: courseName + " tillagd." });
 
     } catch (error) {
-        if (error?.code === "23505"){
+        if (error?.code === "23505") {
             res.status(400).json({ message: "Kurs redan tillagd" });
         } else {
             res.status(500).json({ message: "Serverfel" });
@@ -50,5 +48,34 @@ router.post("/", authenticateToken, async (req, res) => {
         console.error(error);
     }
 });
+
+// Ta bort kurs
+router.delete("/", authenticateToken, async (req, res) => {
+    try {
+        const userId = req.user.id;
+        const { courseCode } = req.body;
+
+        if (!courseCode) {
+            return res.status(400).json({ message: "Välj en kurs!" });
+        }
+
+        const result = await client.query(
+            "DELETE FROM courses WHERE course_code = $1 AND user_id = $2",
+            [courseCode, userId]
+        );
+
+        if (result.rowCount === 0) {
+            return res.status(404).json({ message: "Kurs hittades inte" });
+        }
+
+        res.json({ message: "Kurs borttagen" });
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Serverfel" });
+    }
+
+});
+
 
 module.exports = router;
